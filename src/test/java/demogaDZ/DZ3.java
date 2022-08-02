865a3001a26c55e4daabdb5982d036aca8d8ec5d
19733726 <vitaliy_mayorskiy@mail.ru>
2022-08-02 14:38:57 +0300

    added DZ3

--- src/test/java/demogaDZ/DZ3.java
+++ src/test/java/demogaDZ/DZ3.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -25,14 +26,34 @@ public class DZ3 {
         $("#lastName").setValue("Major");
         $("#userEmail").setValue("Vital@mail.ru");
         $("[for='gender-radio-1']").click();
-        $("#userNumber").setValue("555-35-35");
+        $("#userNumber").setValue("8929555555");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(0);
-        $(".react-datepicker__year-select").selectOption(92);
-        $(".react-datepicker__day--002").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("2021");
+        $(".react-datepicker__day--008").click();
         $("#subjectsInput").setValue("Arts").pressEnter();
-        $("[for='hobbies-checkbox-3']").click();
-        $("#uploadPicture").uploadFile(new File("src/test/demogaDZ/picture/test.png"));
+        $("[for='hobbies-checkbox-1']").click();
+        $("#uploadPicture").uploadFile(new File("src/test/java/picture/test.jpg"));
+        $("#currentAddress").setValue("Pervaya 1");
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(text("Vital Major"));
+        $(".modal-body").shouldHave(text("Vital@mail.ru"));
+        $(".modal-body").shouldHave(text("Male"));
+        $(".modal-body").shouldHave(text("8929555555"));
+        $(".modal-body").shouldHave(text("08 January,2021"));
+        $(".modal-body").shouldHave(text("Arts"));
+        $(".modal-body").shouldHave(text("Sport"));
+        $(".modal-body").shouldHave(text("test.jpg"));
+        $(".modal-body").shouldHave(text("Pervaya 1"));
+        $(".modal-body").shouldHave(text("NCR Delhi"));
+
+        $("#closeLargeModal").click();
 
 
 
